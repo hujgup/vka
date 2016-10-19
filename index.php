0fc7a87976ea5f7613d29dc9f0a985c13ced7dde
hujgup <hujgup@users.noreply.github.com>
2016-10-20 00:43:35 +1100

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Vriska's Kinky Adventure - Main Page</title>
+	<link rel="stylesheet" href="/global.css" />
+	<link rel="stylesheet" href="/navbar/navbar.css" />
+	<script src="/libraries/global.js"></script>
+	<script src="/libraries/previewList.js"></script>
+	<script src="/navbar/navbar.js"></script>
+	<?php
+		require_once($_SERVER["DOCUMENT_ROOT"]."libraries/include.php");
+	?>
+</head>
+<body>
+	<?php
+		f_include($_SERVER["DOCUMENT_ROOT"]."navbar/navbar.htmlt");
+	?>
+	<article class="content">
+		<header>
+			<h1>Vriska's Kinky Adventure</h1>
+			<p>v0.1.0-pre-alpha</p>
+			<p><strong>By Echo</strong></p>
+			<p><a href="http://inhibitionsnullandvoid.tumblr.com">inhibitionsnullandvoid.tumblr.com</a></p>
+		</header>
+		<section>
+			<p><em>VKA</em> is a browser game where you control Vriska Serket as she puts herself and others in incredibly kinky situations. Make choices that determine how events unfold, such as what kinks to pursue, who to fuck, and whether you should have pizza or noodles for dinner tonight.</p>
+			<p>The game will not force you to explore any kink - each path is entirely optional. With that in mind, here are some of the kinks Vriska might indulge in.</p>
+			<ul>
+				<li>Modular/Swappable Body Parts</li>
+				<li>Hard Vore/Cannibalism</li>
+				<li>Masochism/Snuff</li>
+				<li>Rediction/Amputation</li>
+				<li>Transformation (Animal, Body Part, Inanimate)</li>
+				<li>Free Use</li>
+				<li>Objectification</li>
+				<li>And the kinkiest option of all: Vanilla Sex</li>
+			</ul>
+			<p><a href="game">Click here to begin playing.</a></p>
+		</section>
+		<footer>
+			<p>This work is distributed under a <em>Creative Commons Attribution Noncommercial</em> license (CC-BY-NC). A human-readable summary of the license can be found <a target="_blank" href="https://creativecommons.org/licenses/by-nc/4.0/">here</a>. For the full legalese, <a target="_blank" href="https://creativecommons.org/licenses/by-nc/4.0/legalcode">click here</a>.</p>
+		</footer>
+	</article>
+</body>
+</html>
