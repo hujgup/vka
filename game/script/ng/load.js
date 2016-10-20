fc502fd1efcfd7a5fd4c82f1ae50619bd591a2cb
JAKE TUNALEY <jtunaley@gmail.com>
2016-10-20 17:54:32 +1100

    Load structure.

--- game/script/ng/load.js
+++ game/script/ng/load.js
@@ -1,15 +1,10 @@
 /*
 TODO:
 Load all .css files in style/app
-Load all .js files in script/app
-	Look up load order in loadOrder.json, and put them in that order
 */
 
-const Engine = new (function() {
-	var _container;
-	var _containerContent;
-
-	const LoadCounter = function(max,container) {
+var Engine = new (function() {
+	var LoadCounter = function(max,container) {
 		var _loaded = 0;
 		var _max = max;
 		var _errors = [];
@@ -47,15 +42,64 @@ const Engine = new (function() {
 		_render();
 	};
 
+	var _container;
+	var _containerContent;
+	var _localization;
+
+
 	window.addEventListener("DOMContentLoaded",function() {
 		_container = document.getElementById("container");
 		_containerContent = container.innerHTML;
-		const manager = new LoadCounter(1,_container);
+		var manager = new LoadCounter(4,_container);
 		manager.onLoad = function() {
 			_container.innerHTML = _containerContent;
 		};
+		var query = location.search;
+		if (query.length !== 0) {
+			var kvps = query.split("&");
+			var kvp;
+			for (var i = 0; i < kvps.length; i++) {
+				kvp = kvps[i].split("=");
+				query[kvp[0]] = kvp[1];
+			}
+		} else {
+			query = {};
+		}
 		var req = new AJAXRequest(HTTPMethods.POST,"script/ng/io/loadStyling.php");
 		req.execute(function(res) {
+			if (res.error) {
+				manager.error(res.text);
+			} else {
+				manager.increment();
+			}
+		});
+		var req2 = new AJAXRequest(HTTPMethods.POST,"script/ng/io/loadNg.php");
+		req2.data = {
+			file: "common/ng/objects.cfg"
+		};
+		req2.execute(function(res2) {
+			if (res.error) {
+				manager.error(res.text);
+			} else {
+				manager.increment();
+			}
+		});
+		var req3 = new AJAXRequest(HTTPMethods.POST,"script/ng/io/loadNg.php");
+		req3.data = {
+			file: "common/ng/rooms.cfg"
+		};
+		req3.execute(function(res3) {
+			if (res.error) {
+				manager.error(res.text);
+			} else {
+				manager.increment();
+			}
+		});
+		var req4 = new AJAXRequest(HTTPMethods.POST,"script/ng/io/loadLocalization.php");
+		req4.data = {
+			code: query.hasOwnProperty("lang") ? query.lang : "en";
+		};
+		req4.execute(function(res2) {
 			if (res.error) {
 				manager.error(res.text);
 			} else {
